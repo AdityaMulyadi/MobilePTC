c68847a0df64046fd1135bae3855249946f4b029
uzyeu <muhfaruqalfauzi@gmail.com>
2024-12-25 18:57:53 +0800

    bilang prediksi dan jkasifikasi don

--- app/src/main/java/com/example/mobileptc/Home.java
+++ app/src/main/java/com/example/mobileptc/Home.java
@@ -33,12 +33,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.text.SimpleDateFormat;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -51,6 +53,7 @@ import retrofit2.http.GET;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.tensorflow.lite.Interpreter;
+import org.w3c.dom.Text;
 
 
 public class Home extends AppCompatActivity {
@@ -222,58 +225,6 @@ public class Home extends AppCompatActivity {
             }
         });
 
-//        query.addChildEventListener(new ValueChildListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                String status, dataLux;
-//                Integer lux;
-//                Boolean cuaca;
-//
-//                if (snapshot.exists()) {
-//                    lux = snapshot.child("Cahaya").getValue(Integer.class);
-//                    status = snapshot.child("Status_Jemuran").getValue(String.class);
-//                    cuaca = snapshot.child("Hujan").getValue(Boolean.class);
-//
-//                    if (lux != null) {
-//                        dataLux = lux + "cd";
-//                        txtLux.setText(dataLux);
-//                    } else {
-//                        dataLux = "N/A";
-//                        txtLux.setText(dataLux);
-//                    }
-//
-//                    if (status != null) {
-//                        dataStatus.setText(status);
-//                    } else {
-//                        dataStatus.setText("N/A");
-//                    }
-//
-//                    if (cuaca != null) {
-//                        if (cuaca) {
-//                            ikonCuaca.setImageResource(R.drawable.rainyday);
-//                        } else {
-//                            ikonCuaca.setImageResource(R.drawable.sun);
-//                        }
-//                    } else {
-//                        ikonCuaca.setImageResource(R.drawable.fog);
-//                    }
-//                } else {
-//                    dataLux = "N/A";
-//                    dataStatus.setText("N/A");
-//                    ikonCuaca.setImageResource(R.drawable.fog);
-//                    txtLux.setText(dataLux);
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                // Handle errors
-//                Log.e("Firebase", "Error reading data from Firebase: " + error.getMessage());
-//                // Display error message to the user
-//                Toast.makeText(Home.this, "Gagal mengambil data dari Firebase.", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
         Intent intent = getIntent();
         String nama, alamat, email, pass;
         nama = intent.getStringExtra("nama");
@@ -298,23 +249,25 @@ public class Home extends AppCompatActivity {
         controlIoT();
 
         try {
+            TextView txt = findViewById(R.id.textView8);
 //            InputStream inputStream2 = getAssets().open("temp.txt");
 //            AssetFileDescriptor ast = getAssets().openFd("model_lstm.tflite");
 //            WeatherPredict weatherPredict = new WeatherPredict();
 //            PrediksiCuaca prediksiCuaca = new PrediksiCuaca("C:\\Users\\ASUS.LAPTOP-5901OKQB\\AndroidStudioProjects\\MobilePTC\\app\\src\\main\\assets\\model_lstmaftrepb.tflite");
-            float[] data = {20, 10, 15, 13, 11, 6, 5};
+            float[] data = {26.3F, 91, 31.5F, 0.0F, 100, 9.4F, 94};
             float[][] prediksi = prediksiCuaca(data);
+            int klasifikasi = klasifikasiCuaca(prediksi);
+            txt.setText(String.valueOf(klasifikasi));
 //            float[] prediksi = weatherPredict.prediksiCuaca(data);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
-//        predictWeather();
 
     }
 
     public float[][] prediksiCuaca (float[] inputData) throws IOException {
-        Interpreter interpreter = new Interpreter(loadModelFile());
+        Interpreter interpreter = new Interpreter(loadModelFile("weather_prediction.tflite"));
         DescriptiveStatistics stats = new DescriptiveStatistics();
         for (float value : inputData) {
             stats.addValue(value);
@@ -325,29 +278,101 @@ public class Home extends AppCompatActivity {
         for (int i = 0; i < inputData.length; i++) {
             inputData[i] = (float) scale(inputData[i], min, max);
         }
+
+//        byte[] paddedData = new byte[336];
+//        for (int i = 0; i < inputData.length; i++) {
+//            // Konversi nilai float (0.0 - 1.0) ke byte (0 - 255)
+//            paddedData[i] = (byte) inputData[i];
+//        }
+
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(inputData.length * 4);
+//        for (float value : inputData) {
+//            byteBuffer.putFloat(value);
+//        }
+        byteBuffer.order(ByteOrder.nativeOrder());
         for (float value : inputData) {
             byteBuffer.putFloat(value);
         }
+//        byteBuffer.put(paddedData);
 
         float[][] output = new float[1][7];
         interpreter.run(byteBuffer, output);
 
-        return output;
+        return reverseScaleOutput(output, min, max);
     }
 
-    private MappedByteBuffer loadModelFile () throws IOException {
+    public int klasifikasiCuaca(float[][] inputData) throws IOException {
+        Interpreter interpreter = new Interpreter(loadModelFile("weather_classification.tflite"));
+//        DescriptiveStatistics stats = new DescriptiveStatistics();
+//        for (float value : inputData[0]) {
+//            stats.addValue(value);
+//        }
+//        double min = stats.getMin();
+//        double max = stats.getMax();
+//
+//        for (int i = 0; i < inputData[0].length; i++) {
+//            inputData[0][i] = (float) scale(inputData[0][i], min, max);
+//        }
+
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(inputData[0].length * 4);
+        byteBuffer.order(ByteOrder.nativeOrder());
+        for (float value : inputData[0]) {
+            byteBuffer.putFloat(value);
+        }
+
+        ByteBuffer outputBuffer = ByteBuffer.allocateDirect(4 * 66);
+        outputBuffer.order(ByteOrder.nativeOrder());
+
+
+        interpreter.run(byteBuffer, outputBuffer);
+
+
+        outputBuffer.rewind();
+        float[] probabilities = new float[66];
+        outputBuffer.asFloatBuffer().get(probabilities);
+
+
+        int predictedClass = -1;
+        float maxProbability = Float.MIN_VALUE;
+        for (int i = 0; i < probabilities.length; i++) {
+            if (probabilities[i] > maxProbability) {
+                maxProbability = probabilities[i];
+                predictedClass = i;
+            }
+        }
+
+        return predictedClass;
+    }
+
+    private MappedByteBuffer loadModelFile (String path) throws IOException {
         try {
-            AssetFileDescriptor fileDescriptor = getAssets().openFd("model_lstm.tflite");
+            AssetFileDescriptor fileDescriptor = getAssets().openFd(path);
             FileInputStream inputStream = fileDescriptor.createInputStream();
             FileChannel fileChannel = inputStream.getChannel();
             long startOffset = fileDescriptor.getStartOffset();
             long declaredLength = fileDescriptor.getDeclaredLength();
             return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
         } catch (IOException e) {
-            Log.e("Home", "Error membuka file model_lstm.tflite", e);
+            Log.e("Home", "Error membuka file " + path, e);
             throw new IOException("Error loading model file", e);
         }
+    }
+
+    private float reverseScale(float value, double min, double max) {
+        return (float) (value * (max - min) + min);
+    }
+
+    public float[][] reverseScaleOutput(float[][] scaledOutput, double min, double max) {
+        int rows = scaledOutput.length;
+        int cols = scaledOutput[0].length;
+        float[][] reversedOutput = new float[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                reversedOutput[i][j] = reverseScale(scaledOutput[i][j], min, max);
+            }
+        }
+        return reversedOutput;
     }
 
     public interface OpenMeteoService {
@@ -489,189 +514,9 @@ public class Home extends AppCompatActivity {
     }
 
 
-//    private void weatherPrdct() throws IOException {
-//        Interpreter interpreter = new Interpreter(loadModelFile());
-//    }
-
-
-
-//    private void predictWeather() {
-
-
-//        try {
-//            float[] data = {20, 10, 15, 13, 11, 6, 5};
-//
-//            DescriptiveStatistics stats = new DescriptiveStatistics();
-//            for (float value : data) {
-//                stats.addValue(value);
-//            }
-//            double min = stats.getMin();
-//            double max = stats.getMax();
-//
-//            for (int i = 0; i < data.length; i++) {
-//                data[i] = (float) scale(data[i], min, max);
-//            }
-//            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(data.length * 4);
-//            for (float value : data) {
-//                byteBuffer.putFloat(value);
-//            }
-//            ModelLstm40SudahDariPb model = ModelLstm40SudahDariPb.newInstance(Home.this);
-//
-//            // Creates inputs for reference.
-////            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 12, 7}, DataType.FLOAT32);
-////            inputFeature0.loadBuffer(byteBuffer);
-//
-//            // Runs model inference and gets result.
-////            ModelLstm40SudahDariPb.Outputs outputs = model.process(inputFeature0);
-////            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
-//
-//            // Releases model resources if no longer used.
-//            model.close();
-//        } catch (IOException e) {
-//            // TODO Handle the exception
-////            startActivity(new Intent(Home.this, MainActivity.class));
-//        }
-//        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.open-meteo.com/").addConverterFactory(GsonConverterFactory.create()).build();
-//        OpenMeteoService service = retrofit.create(OpenMeteoService.class);
-//        Call<OpenMeteoResponse> call = service.getWeatherData();
-//
-//        call.enqueue(new Callback<OpenMeteoResponse>() {
-//            @Override
-//            public void onResponse(Call<OpenMeteoResponse> call, Response<OpenMeteoResponse> response) {
-//                if (response.isSuccessful()) {
-////                    OpenMeteoResponse weatherData = response.body();
-////                    float temp2m, lembap, tempAp, rain, ws10m, precip, cc, wd10m, weatherCode;
-////
-////                    assert weatherData != null;
-////                    temp2m = (float) weatherData.currentWeather.temperature;
-////                    lembap = (float) weatherData.currentWeather.kelembapan;
-////                    tempAp = (float) weatherData.currentWeather.appTemp;
-////                    rain = (float) weatherData.currentWeather.rain;
-////                    ws10m = (float) weatherData.currentWeather.kecAngin;
-////                    precip = (float) weatherData.currentWeather.curah;
-////                    cc = (float) weatherData.currentWeather.cloud_cover;
-////                    wd10m = (float) weatherData.currentWeather.arah_angin;
-////                    weatherCode = (float) weatherData.currentWeather.kode_cuaca;
-////
-////                    float[] data = {temp2m, lembap, tempAp, rain, ws10m, cc, wd10m};
-////
-////                    DescriptiveStatistics stats = new DescriptiveStatistics();
-////                    for (float value : data) {
-////                        stats.addValue(value);
-////                    }
-////                    double min = stats.getMin();
-////                    double max = stats.getMax();
-////
-////                    for (int i = 0; i < data.length; i++) {
-////                        data[i] = (float) scale(data[i], min, max);
-////                    }
-////
-////
-////                    ByteBuffer byteBuffer = ByteBuffer.allocateDirect(data.length * 4);
-////                    for (float value : data) {
-////                        byteBuffer.putFloat(value);
-////                    }
-//                    try {
-//                        Modellstmregresi model = Modellstmregresi.newInstance(Home.this);
-//
-//                        // Creates inputs for reference.
-////                        TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 12, 7}, DataType.FLOAT32);
-////                        inputFeature0.loadBuffer(byteBuffer);
-////
-////                        // Runs model inference and gets result.
-////                        Modellstmregresi.Outputs outputs = model.process(inputFeature0);
-////                        TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
-//
-//                        // Releases model resources if no longer used.
-//                        model.close();
-//                    } catch (IOException e) {
-//                        // TODO Handle the exception
-//                        startActivity(new Intent(Home.this, MainActivity.class));
-//                    }
-////                    try {
-////
-//
-//
-////                        ConvertedModellstm3 model = ConvertedModellstm3.newInstance(Home.this);
-//
-//
-////                        TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 12, 9}, DataType.FLOAT32);
-////                        inputFeature0.loadBuffer(byteBuffer);
-////
-////
-////                        ConvertedModellstm3.Outputs outputs = model.process(inputFeature0);
-////                        TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
-////                        float[] out = outputFeature0.getFloatArray();
-//
-////                        TextView txt8 = findViewById(R.id.textView8);
-////                        Integer out1 = (int) out[0];
-////                        txt8.setText(out1.toString());
-//
-//                        // Releases model resources if no longer used.
-////                        model.close();
-////                    }
-////                    catch (IOException e) {
-////                        // TODO Handle the exception
-////                        Toast.makeText(Home.this, "Error ML", Toast.LENGTH_SHORT).show();
-////                    }
-////                    // Load model
-////                    try (MappedByteBuffer byteBuffer = FileUtil.loadMappedFile(assets, "C:\\Users\\ASUS.LAPTOP-5901OKQB\\AndroidStudioProjects\\MobilePTC\\app\\src\\main\\assets\\model_lstm3.h5")) {
-////                        Interpreter interpreter = new Interpreter(byteBuffer);
-////
-////                        // Prepare input data
-////                        // Prepare input data
-////                        double[] inputData = {temp2m, lembap, tempAp, rain, ws10m, precip, cc, wd10m, weatherCode}; // Ganti dengan data cuaca aktual
-////                        Tensor inputTensor = (Tensor) TensorBuffer.createFixedSize(new int[]{1, inputData.length}, DataType.FLOAT32);
-////                        inputTensor.loadArray(inputData);
-////
-////                        // Run inference
-//////                        float[][] output = new float[1][2]; // Output: [probabilitas tidak hujan, probabilitas hujan]
-//////                        interpreter.run(inputTensor, output);
-//////
-//////                        // Process output
-//////                        float probabilityOfRainTomorrow = output[0][1];
-//////                        float probabilityOfRainDayAfterTomorrow = output[1][1];
-//////
-//////                        // Display result
-//////                        if (probabilityOfRainTomorrow > 0.5) {
-//////                            textView.setText("Besok kemungkinan hujan");
-//////                        } else {
-//////                            textView.setText("Besok kemungkinan tidak hujan");
-//////                        }
-////                        // Lakukan hal yang sama untuk besok lusa
-////                    } catch (IOException e) {
-////                        // Handle error
-////                        Toast.makeText(Home.this, "Kesalahan pada Sistem cerdas", Toast.LENGTH_SHORT).show();
-////                    }
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<OpenMeteoResponse> call, Throwable t) {
-//                Log.e("NetworkError", "Network Error: " + t.getMessage());
-//                Toast.makeText(Home.this, "Failed to fetch weather data. Please try again later.", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-//    }
-
     private double scale(double value, double min, double max) {
         return (value - min) / (max - min);
     }
 
 }
-
-
-//ByteBuffer byteBuffer = ByteBuffer.allocateDirect(9*4);
-//                        byteBuffer.putFloat(temp2m);
-//                        byteBuffer.putFloat(lembap);
-//                        byteBuffer.putFloat(tempAp);
-//                        byteBuffer.putFloat(rain);
-//                        byteBuffer.putFloat(ws10m);
-//                        byteBuffer.putFloat(precip);
-//                        byteBuffer.putFloat(cc);
-//                        byteBuffer.putFloat(wd10m);
-//                        byteBuffer.putFloat(weatherCode);
-
-//                        MinMaxScaler scaler = "C:\\Users\\ASUS.LAPTOP-5901OKQB\\AndroidStudioProjects\\MobilePTC\\app\\src\\main\\assets\\scaler3.pkl";
 
